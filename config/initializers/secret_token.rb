e6deb755b3f1c6cf93c3b69ebdc6ce789069fe30
Yann Klis <yann.klis@novelys.com>
2012-05-25 21:39:26 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tribune::Application.config.secret_token = '4ae6c274195e129880ddf9890e6d199392aa62f12983e52f3e5ec0f173c046636ea7661315ca04f1750c8d2548a0c8cba6acc29ca77f5c870144273ef51731f1'
